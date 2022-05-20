03ab1d98a5be12d0f50e7ad62c7c412a22bae246
superdaggler <ndbellew@gmail.com>
2022-05-19 22:09:10 -0600

    yo

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    REDDIT_SECRET="2IUrnnBtd1Zw6sWs3y9OeQmHadC9lg"
+    REDDIT_CLIENT_ID="v1FG8t0fLzXEKbizv27mAw"
+    REDDIT_DEVELOPERS={"Head":"SuperDaggler"}
+    REDDIT_USERNAME=""
+    REDDIT_PASSWORD=""
